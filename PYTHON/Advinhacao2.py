343564c7c6416a97f55a6f321e94a1b4016302c2
Guerreiro015 <antoniolucasluana@gmail.com>
2023-06-01 19:56:33 -0300

    atualiza

--- PYTHON/Advinhacao2.py
+++ PYTHON/Advinhacao2.py
@@ -2,23 +2,24 @@ print("*"*50)
 print("* Bem vindo ao jogo de Adivinhação *".center(50))
 print("*"*50)
 tentativas=1     
-while(tentativas==1):
+while(tentativas !=0 ):
     num = 42
     chute = input("Digite o seu CHUTE ..:  ")
     chute = int(chute)
 
 
     if (chute == num):
-        print('Você acertou, o numero foi ',chute)
-
+        print('Você acertou na {}ª tentativa o numero é '.format(tentativas),chute)
         print("-"*50)
         print("Fim do jogo ".center(50))
         print("*"*50)
         tentativas=0
-    elif (chute > num):
-            print('\nNão deu, você errou - -  o número é menor....\n')
 
+    elif (chute > num):
+            print('\n {}ª tentativa errada - -  o número é menor que {}'.format(tentativas,chute))
+            tentativas=tentativas+1
     elif (chute < num):
-            print('\nNão deu, você errou - -  o número é maior....\n')
+            print('\n {}ª tentativa errada - -  o número é maior que {}'.format(tentativas,chute))
+            tentativas=tentativas+1
     else:
             print('erro desconhecido....')
