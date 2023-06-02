06039e1904ffabe6dad60ef60bf21b172f528f78
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-02 12:51:46 -0300

    Update Cálculo_INSS_IRRF.py

--- PYTHON/Cálculo_INSS_IRRF.py
+++ PYTHON/Cálculo_INSS_IRRF.py
@@ -11,6 +11,7 @@ fal=int(input('Digite Valor das faltas e atrasos. .: '))
 pen=int(input('Digite o valor da Pensão............: '))
 dep=int(input('Digite a Quant. de Dependentes. IR..: '))
 dep=dep*189.59
+deducao_simplicada = 528.00
 
 #------------------------------------------------------------
 basesalario = [1.320, 2571.29, 3856.94, 7507.49]
@@ -19,7 +20,7 @@ aliquotainss = [7.5, 9, 12, 14]
 
 deducaoinss = [0, 19.8, 96.94, 174.08]
 
-tetoinss = 876,95
+tetoinss = 876.95
 
 baseir = [2112.01, 2826.66, 3751.06, 5000000]
 
@@ -37,23 +38,35 @@ sal=(sal-fal)
 fgts=sal*0.08
 inss=0
 cont = 0
+
+print(basesalario[3])
+print(sal)
 for i in range(cont < len(basesalario)):
     if sal > basesalario[3]:
-      int(inss = tetoinss)
+      inss = tetoinss
       break
     
     else:
         if sal < basesalario[cont]:
-            int(inss = (sal*aliquotainss[cont])-parceladeduzir[cont])
+            inss = (sal*aliquotainss[cont])-parceladeduzir[cont]
+            
             break
     cont = cont+1
 
 
 print("="*50)
 print(f'Valor base do INSS é : {sal:,.2f}')
+print(f'O valor do INSS será..: {inss:,.2f}')
+print("="*50)
 #--------------------------------------------------------
-baseir=sal-(pen+dep+inss)
-if baseir<1903.99:
+
+base_deducao_ir = (pen+dep+inss)
+if (base_deducao_ir > deducao_simplicada):
+        base_deducao_ir = deducao_simplicada
+        
+baseir=sal-base_deducao_ir
+
+if baseir<2112.01:
   ir=0
 elif baseir<2826.66:
   ir=baseir*0.075-142.8
@@ -64,8 +77,7 @@ elif baseir<4664.69:
 else:
   ir=baseir*0.275-869.36
 
-print(f'O valor do INSS será..: {inss:,.2f}')
-print("="*50)
+
 print(f'O Valor do FGTS é ....: {fgts:,.2f}')
 print("="*50)
 print(f'O Base do IRRF  é ....: {baseir:,.2f}')
