9c3387c347e4fb1ebf5b5685a3899bbf2001bb99
Guerreiro015 <antoniolucasluana@gmail.com>
2023-06-04 13:12:00 -0300

    atua

--- PYTHON/titulo_menu.py
+++ PYTHON/titulo_menu.py
@@ -2,3 +2,12 @@ def menu():
     print("*"*50)
     print("****  PREPARE SE PARA JOGAR  *****".center(50))
     print("*"*50)
+
+# O prgrama acima não consegue ser chamado sozinho
+# para executar diretamante precisamos colocar o codifo abaixo no final
+
+if (__name__ == "__main__"):
+    menu()
+    
+# desta maneira ele pode ser chamado em outro programa e também pode 
+# ser usado sozinho
