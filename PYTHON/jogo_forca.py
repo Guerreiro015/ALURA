180bd94e571fc207b39506d5e4ddf202a710298c
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-05 12:51:15 -0300

    Update jogo_forca.py

--- PYTHON/jogo_forca.py
+++ PYTHON/jogo_forca.py
@@ -3,14 +3,24 @@ def jogar():
     print('****** Bem-vindo ao jogo da Forca *******')
     print('*****************************************')
 
-    palavra_secreta = "banana"
+    palavra = "banana"
     enforcou = False
     acertou = False
+    index = 0
 
     while(not enforcou and not acertou):
         print("jogando...")
+        chute = input("Qual chute? ")
+        for letra in (palavra):
+            if (chute==letra):
+                print(f"A letra ( {letra} ) digitada foi encontrda na posição ( {index} )")
+                index=index+1
+            else:
+                print(f"A letra ( {chute}) digitada, não tem na palavra secreta")
+                index=index+1
         acertou = True
     print("Fim do jogo")
+        
 
 if(__name__ == '__main__'):
     jogar()
