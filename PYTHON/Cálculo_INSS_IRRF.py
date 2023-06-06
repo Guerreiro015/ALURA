fd3e9056dfa80933586b24562bfd5799c4d53ca5
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-06 06:53:08 -0300

    atualizacao

--- PYTHON/Cálculo_INSS_IRRF.py
+++ PYTHON/Cálculo_INSS_IRRF.py
@@ -1,3 +1,6 @@
+import os
+os.system('cls')
+
 def linha():
   print("="*50)
 
--- PYTHON/jogo_forca.py
+++ PYTHON/jogo_forca.py
@@ -1,21 +1,27 @@
-def jogar():
+def forca():
+    import os
+    os.system('cls') or None
+
     print('*****************************************')
     print('****** Bem-vindo ao jogo da Forca *******')
     print('*****************************************')
 
+    index = 0
     palavra = " BANANA   "
-    enforcou = False
-    acertou = False
+    lista = ["_","_","_","_","_","_",]
+
     palavra = palavra.upper()# Transformar em maisculos
     palavra = palavra.lower()# Transformar em minúsculo
     palavra = palavra.strip() # tira espaços no inicio e fim
-    
-    print(palavra)   
+    palavra = palavra.capitalize() # Primeira letra em maiúscula
+     
 
-    index = 0
 
+    enforcou = False
+    acertou = False
     while(not enforcou and not acertou):
-        print("jogando...")
+        print(lista[0],lista[1],lista[2],lista[3],lista[4],lista[5],"\n")
+        print("Acerte a palavra acima...")
         chute = input("Qual chute? ")
         chute = chute.strip()
         chute = chute.lower()
@@ -23,14 +29,17 @@ def jogar():
         for letra in (palavra):
             if (chute.lower() == letra.lower()):
                 print(f"A letra ( {letra} ) digitada foi encontrda na posição ( {index} )")
+                lista[index] = chute
                 index=index+1
             else:
                # print(f"A letra ( {chute}) digitada, não tem na palavra secreta")
                 index=index+1
                 continue
-        acertou = True
+    
+        index=0   
+        acertou = False
     print("Fim do jogo")
         
 
 if(__name__ == '__main__'):
-    jogar()
+    forca()
