8fb27df378df24d35293e3685bbfb669e201e697
Guerreiro015 <antoniolucasluana@gmail.com>
2023-06-05 22:14:28 -0300

    Update jogo_forca.py

--- PYTHON/jogo_forca.py
+++ PYTHON/jogo_forca.py
@@ -3,21 +3,25 @@ def jogar():
     print('****** Bem-vindo ao jogo da Forca *******')
     print('*****************************************')
 
-    palavra = "banana"
+    palavra = " BANANA   "
     enforcou = False
     acertou = False
-    maiusculo = palavra.upper()# Transformar em maisculos
-    minusculo = palavra.lower()# Transformar em minúsculo
-    print(maiusculo)
-    print(minusculo)
+    palavra = palavra.upper()# Transformar em maisculos
+    palavra = palavra.lower()# Transformar em minúsculo
+    palavra = palavra.strip() # tira espaços no inicio e fim
+    
+    print(palavra)   
 
     index = 0
 
     while(not enforcou and not acertou):
         print("jogando...")
         chute = input("Qual chute? ")
+        chute = chute.strip()
+        chute = chute.lower()
+
         for letra in (palavra):
-            if (chute==letra):
+            if (chute.lower() == letra.lower()):
                 print(f"A letra ( {letra} ) digitada foi encontrda na posição ( {index} )")
                 index=index+1
             else:
