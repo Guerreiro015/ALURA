0c9bb396e84b786b0751d632c4b58efa00e1a8d5
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-07 07:25:40 -0300

    atualização

--- PYTHON/Advinhacao_Menu.py
+++ PYTHON/Advinhacao_Menu.py
@@ -1,3 +1,4 @@
+import random
 import os
 os.system('cls') or None
 
@@ -8,22 +9,30 @@ titulo_menu.menu() # usando funcao que esta no programa titulo_menu
 def jogo1():
     import advinhacao_randon # executando este programa
 def jogo2():
-    import Advinhacao_While # executando este programa
+    import Advinhacao_while # executando este programa
+def jogo3():
+    import jogo_forca # executando este programa
+def jogo4():
+    import Cálculo_INSS_IRRF # executando este programa
 
-
-import random
-
-print("*"*50)
-print("****  MENU DOS JOGOS DE ADVINHAR!  *****".center(50))
-print("*"*50)
 
 cont = True
 
 while cont == True:
+    os.system("cls" or None)
+    print("*"*50)
+    print("****  MENU DOS JOGOS DE ADVINHAR!  *****".center(50))
+    print("*"*50)
 
     print("-"*50)
     print('Escolha abaixo qual jogo deseja jogar'.center(50))
-    opcao = input("\n (1) - ADVINHACAO RANDOM (2) - ADVINHACAO WHILE  " )
+    print("-"*50)
+    print("\n (1) - Advinhação Random *** (2) - Advinhação com While ")
+    print("\n (3) - Jogo da Forca  ****** (4) - Cáculo de INSS e IRRF")
+    print("-"*50)
+
+    opcao = input("\n Escolha qual jogo você quer jogar ..: ")
+
     if opcao == "":
         continue
     opcao = int(opcao)
@@ -31,6 +40,11 @@ while cont == True:
         jogo1()
     elif opcao == 2:
         jogo2()
+    elif opcao == 3:
+        jogo3()
+    elif opcao == 4:
+        jogo4()
+
     else:
         print("Opçâo inválida, Tchau")
         break
