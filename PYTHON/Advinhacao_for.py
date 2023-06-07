a0b575f4a48dc3e466285391546c66b3d4b416a9
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-07 07:04:59 -0300

    atua

--- PYTHON/Advinhacao_for.py
+++ PYTHON/Advinhacao_for.py
@@ -1,9 +1,10 @@
 
 def listra(): 
     print ("*"*50) 
-
-listra()
-print("* Bem vindo ao jogo de Adivinhação *".center(50))
+    print("* Bem vindo ao jogo de Adivinhação *".center(50))
+    print ("*"*50) 
+import os
+os.system("cls") or None
 listra()
 
 num = 42
@@ -12,23 +13,28 @@ for i in range(chance, 10):
     chute = input("Digite o seu CHUTE ..:  ")
     chute = int(chute)
     if (chute == num):
-        print('Você acertou, na chance {}, o numero é '.format(chance),chute)
 
-        listra()
+        print("-"*50)
+        print('Você acertou, na chance {}, o numero é '.format(chance),chute)
+        print("-"*50)
+       
         print("Fim do jogo ".center(50))
-        listra()
+        print("-"*50)
         break
 
     elif (chute > num):
-        listra()
         print('\n A Chance {}, chute {} Não deu, você errou - -  o número é MENOR\n'.format(chance,chute))
+        input()
+        os.system("cls") or None
         listra()
         chance = chance+1
 
     elif (chute < num):
-        listra()
         print('\n A Chance {}, chute {} Não deu, você errou - -  o número é MAIOR\n'.format(chance,chute))
+        input()
+        os.system("cls") or None
         listra()
+        chance = chance+1
         chance = chance+1
 
     else:
--- PYTHON/advinhacao_randon.py
+++ PYTHON/advinhacao_randon.py
@@ -1,5 +1,5 @@
 def listra(): 
-    print ("*"*50) 
+    print ("*"*50)
     print("* Bem vindo ao jogo de Adivinhação RANDOM *".center(50))
     print ("*"*50) 
 listra()
@@ -19,9 +19,6 @@ import random
 num = round(random.random()*10)
 num2 = round(random.randrange(101))
 num3 = round(random.randrange(1,5))
-#print (num)
-print (num2)
-print (num3)
 
 tentativas=1     
 while(tentativas !=0 ): 
@@ -34,17 +31,24 @@ while(tentativas !=0 ):
 
 
     if (chute == num):
+            print("-"*50)
             print('Você acertou na {}ª tentativa o numero é '.format(tentativas),chute)
             print("-"*50)
             print("Fim do jogo ".center(50))
-            print("*"*50)
+            print("*"*50,"\n")
             tentativas=0
 
     elif (chute > num):
                 print('\n {}ª tentativa errada - -  o número é menor que {}'.format(tentativas,chute))
+                input()
+                os.system("cls") or None
+                listra()
                 tentativas=tentativas+1
     elif (chute < num):
                 print('\n {}ª tentativa errada - -  o número é maior que {}'.format(tentativas,chute))
+                input()
+                os.system("cls") or None
+                listra()
                 tentativas=tentativas+1
     else:
                 print('erro desconhecido....')
