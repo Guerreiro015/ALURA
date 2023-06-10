4f1661d5b2ccb1145a8f40bd3de21343d77c2b30
Guerreiro015 <antoniolucasluana@gmail.com>
2023-06-09 21:39:04 -0300

    atualiza

--- PYTHON/Advinhacao_for.py
+++ PYTHON/Advinhacao_for.py
@@ -1,9 +1,11 @@
-
-def listra(): 
-    print ("*"*50) 
+def listra():
+    print("*" * 50)
     print("* Bem vindo ao jogo de Adivinhação *".center(50))
-    print ("*"*50) 
+    print("*" * 50)
+
+
 import os
+
 os.system("cls") or None
 listra()
 
@@ -12,34 +14,37 @@ chance = 1
 for i in range(chance, 10):
     chute = input("Digite o seu CHUTE ..:  ")
     chute = int(chute)
-    if (chute == num):
+    if chute == num:
+        print("-" * 50)
+        print("Você acertou, na chance {}, o numero é ".format(chance), chute)
+        print("-" * 50)
 
-        print("-"*50)
-        print('Você acertou, na chance {}, o numero é '.format(chance),chute)
-        print("-"*50)
-       
         print("Fim do jogo ".center(50))
-        print("-"*50)
+        print("-" * 50)
         break
 
-    elif (chute > num):
-        print('\n A Chance {}, chute {} Não deu, você errou - -  o número é MENOR\n'.format(chance,chute))
+    elif chute > num:
+        print(
+            "\n A Chance {}, chute {} Não deu, você errou - -  o número é MENOR\n".format(
+                chance, chute
+            )
+        )
         input()
         os.system("cls") or None
         listra()
-        chance = chance+1
+        chance = chance + 1
 
-    elif (chute < num):
-        print('\n A Chance {}, chute {} Não deu, você errou - -  o número é MAIOR\n'.format(chance,chute))
+    elif chute < num:
+        print(
+            "\n A Chance {}, chute {} Não deu, você errou - -  o número é MAIOR\n".format(
+                chance, chute
+            )
+        )
         input()
         os.system("cls") or None
         listra()
-        chance = chance+1
-        chance = chance+1
+        chance = chance + 1
+        chance = chance + 1
 
     else:
-        print('erro desconhecido....')
-    
-
-
-
+        print("erro desconhecido....")
--- PYTHON/jogo_forca.py
+++ PYTHON/jogo_forca.py
@@ -1,88 +1,89 @@
 def forca():
     import titulo_menu
     import os
-    os.system('cls') or None
+
+    os.system("cls") or None
     titulo_menu.menu()
-    
+
     def perdeu():
-        print("="*50)
+        print("=" * 50)
         print(f"INFELIZMENTE, voce NÃO acertou nas {tentativa-1} tentativas \n")
-        print('Você foi Enforcado'.center(50))  
+        print("Você foi Enforcado".center(50))
         print(f"\nA palavra é - {palavra.upper()} -".center(50))
-        print("="*50)
+        print("=" * 50)
         print("\n Fim do jogo\n")
-        print("="*50)
-        fruta == lista  
-       
-    
+        print("=" * 50)
+        fruta == lista
+
     posicao = 0
-    palavra = "maracuja".upper()
+    digitada = []
+    palavra = "abacaxi".upper()
     tamanho = len(palavra)
     lista = []
-    for i in range(tamanho):# adiconar _ na lista
+    for i in range(tamanho):  # adiconar _ na lista
         lista.append("_")
-        
-    tentativa = 1
-    palavra = palavra.lower()# Transformar em maisculos
-    palavra = palavra.upper()# Transformar em minúsculo
-    palavra = palavra.strip() # tira espaços no inicio e fim
-    
-    #palavra.index(item, inicio, fim) Para saber a posicao de um item dentro da lista
 
-    #palavra = palavra.capitalize() # Primeira letra em maiúscula
+    tentativa = 0
+    palavra = palavra.lower()  # Transformar em maisculos
+    palavra = palavra.upper()  # Transformar em minúsculo
+    palavra = palavra.strip()  # tira espaços no inicio e fim
 
-    #A funcao " ".join serve para imprimir um lista sem os colchetes
+    # palavra.index(item, inicio, fim) Para saber a posicao de um item dentro da lista
 
-    #A função .count() contar o número de ocorrências de um determinado elemento em uma lista.
+    # palavra = palavra.capitalize() # Primeira letra em maiúscula
+
+    # A funcao " ".join serve para imprimir um lista sem os colchetes
+
+    # A função .count() contar o número de ocorrências de um determinado elemento em uma lista.
 
     fruta = list(palavra)
-    print(" ".join(fruta),tamanho)
+    print(" ".join(fruta), tamanho)
 
-    
-    while(fruta != lista):
-        os.system('cls') or None
+    while fruta != lista:
+        tentativa += 1
+        if tentativa > 10:
+            perdeu()
+            break
+        os.system("cls") or None
         titulo_menu.menu()
 
-        print(" ".join(lista))
+        print("PALAVRA SECRETA =>  ", " ".join(lista))
+        print("LETRAS CHUTADAS =>  ", " ".join(digitada))
         print("\nAcerte a palavra acima...")
-        print('-'*50)
+        print("-" * 50)
         chute = input(f"{tentativa}ª Tentativa,  Qual o seu chute? ")
-        print('-'*50)
-
+        print("-" * 50)
         chute = chute.strip().upper()
-        
-        if chute not in(fruta):
-           print(f"A letra digitada, não tem na palavra secreta") 
-           tentativa += 1
-           if tentativa > 5:
-               perdeu()
-               break
-           else:
-            input('')
+        digitada.append(chute)
+
+        if chute not in (fruta):
+            print(f"A letra digitada, não tem na palavra secreta")
+            input("")
             continue
-        for letra in (palavra):
-            if (chute.upper() == letra.upper()):
+
+        for letra in palavra:
+            if chute.upper() == letra.upper():
                 lista[posicao] = chute
                 print(f" A letra  digitada foi encontrada na posição ( {posicao} )")
                 print(" ".join(lista))
-                input('')            
+                input("")
                 posicao += 1
-                
+
             else:
-                posicao +=1
+                posicao += 1
         if fruta == lista:
-
-            print("="*50)
-            print(f"PARABENS, voce acertou em {tentativa-1} tentativas \n")  
+            print("=" * 50)
+            print(f"PARABENS, voce acertou em {tentativa} tentativas \n")
             print(f"A palavra é - {palavra.upper()} -".center(50))
-            print("="*50)
-            print("\n Fim do jogo\n")
-            print("="*50)
+            print("=" * 50)
+            print(" Fim do jogo".center(50))
+            print("=" * 50)
             break
-        
-        tentativa += 1
-                  
-        posicao=0   
 
-if(__name__ == '__main__'):
+        # tentativa += 1
+
+        posicao = 0
+
+
+if __name__ == "__main__":
     forca()
