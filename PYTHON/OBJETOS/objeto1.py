d950aebfbab532309d4013001e508052c846f18b
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-13 06:48:50 -0300

    criacao

---
+++ PYTHON/OBJETOS/objeto1.py
@@ -0,0 +1,23 @@
+numero = 123
+titular = "Nico"
+saldo = 55.0
+limite = 1000.0
+conta = {"numero": 123, "titular": "Nico", "saldo": 55.0, "limite": 1000.0}
+
+conta2 = {"nunero": 222, "titular": "Anto", "saldo": 100, "limite": 1000.0}
+
+def cria_conta(num,nom,sal,lim):
+     conta = {"numero": num, "titular": nom, "saldo": sal, "limite": lim}
+     return conta
+
+teste = cria_conta(123,"antonio",1500,5000)
+
+print(teste)
+
+
+
+
+
+
+
+
---
+++ PYTHON/OBJETOS/teste_conta.py
@@ -0,0 +1,10 @@
+from objeto1 import cria_conta
+
+conta = cria_conta(333,"antonio",500,10000)
+
+print(conta)
+
+banco = cria_conta(444,"francisca",100,1000)
+
+#print(conta)
+print(banco)
