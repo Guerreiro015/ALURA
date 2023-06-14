d603463adfe81217eb4b51eaad2a41adb89f0f61
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-14 08:26:32 -0300

    atualiza

--- PYTHON/OBJETOS/classe.py
+++ PYTHON/OBJETOS/classe.py
@@ -8,9 +8,14 @@ class funci():
 
     def meta(self,meta):
         if self.vendas > meta:
-            print(f" A meta de {self.nome},Bateu meta")
+            print(f" A meta do {self.nome}, era {meta} e o mesmo fez {self.vendas} então ele bateu meta")
         else:
-            print(self.nome,"Não bateu meta")
+             print(f" A meta do {self.nome},era {meta} e o mesmo fez {self.vendas} então ele NÃO bateu meta")
+
+vendedor0 = funci("Francisca")
+vendedor0.vendeu(200)
+vendedor0.meta(500)
+
 
 
 
