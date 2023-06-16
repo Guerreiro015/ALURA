fd84cc5091bdad17ccc70efe07b6600b4b1de2b4
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-16 12:51:02 -0300

    Update datas.py

--- PYTHON/OBJETOS/datas.py
+++ PYTHON/OBJETOS/datas.py
@@ -1,13 +1,20 @@
 class data():
+    import os
+    os.system("cls")
     def __init__(self,dia,mes,ano):
         self.dia = dia
         self.mes = mes
         self.ano = ano
     
-    def data_formatada(d,m,a):
-    
-        print(d,"/",m,"/",a)
+   
+    def data_formadada(self):
+      print (self.dia,"/",self.mes,"/",self.ano)
+     
 
-data.data_formadada(25,11,2017)
+d=data(21,11,2007)
+d.data_formadada()
+
+
+
 
 
