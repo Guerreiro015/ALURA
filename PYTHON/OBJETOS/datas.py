911557bb73a7386f616a5cca29b9243d79cbd6fc
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-16 07:01:17 -0300

    Create datas.py

---
+++ PYTHON/OBJETOS/datas.py
@@ -0,0 +1,13 @@
+class data():
+    def __init__(self,dia,mes,ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+    
+    def data_formatada(d,m,a):
+    
+        print(d,"/",m,"/",a)
+
+data.data_formadada(25,11,2017)
+
+
