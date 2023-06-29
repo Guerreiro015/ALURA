677e8594b9183d88ffaf29ec9ac0d922ab657cce
Guerreiro015 <antoniolucasluana@gmail.com>
2023-06-29 19:48:56 -0300

    ATUILLZA

--- PYTHON/OBJETOS/aula12_multiplas_classes.py
+++ PYTHON/OBJETOS/aula12_multiplas_classes.py
@@ -1,9 +1,11 @@
-
 # utilizando métodos de varias classes
 
 import os
 os.system("cls")
 class Funcionario:
+    def __init__(self,nome):
+        self.nome = nome
+
     def registra_horas(self, horas):
         print(f'Horas registradas...: {horas}\n')
 
@@ -24,20 +26,29 @@ class Alura(Funcionario):
     def busca_perguntas_sem_resposta(self):
         print('Mostrando perguntas não respondidas do fórum')
 
+class hispter():
+    def __str__(self):
+        return f'Meu {self.nome}'
+    
+
 class junior(Alura):
     pass
 
 class pleno(Alura,Caelum):
     pass
 
+class senior(Alura,Caelum,hispter):
+    pass
 
-jose = junior()
+jose = junior("Carlos")
 jose.busca_perguntas_sem_resposta()
 jose.mostrar_tarefas()
 jose.registra_horas("12:00")
 
-luan = pleno()
+luan = pleno("tesouro")
 luan.busca_perguntas_sem_resposta()
 luan.busca_cursos_do_mes("Python")
 luan.registra_horas("13:00")
 
+pedro= senior("garoto")
+print(pedro)
