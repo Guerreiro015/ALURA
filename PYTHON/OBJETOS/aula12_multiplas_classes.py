628b271e79cad7ae9368d4160824802467264b9c
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-06-30 08:38:16 -0300

    Update aula12_multiplas_classes.py

--- PYTHON/OBJETOS/aula12_multiplas_classes.py
+++ PYTHON/OBJETOS/aula12_multiplas_classes.py
@@ -3,10 +3,10 @@
 import os
 os.system("cls")
 class Funcionario:
-    def __init__(self,nome):
+    def __init__(self,nome=None):
         self.nome = nome
 
-    def registra_horas(self, horas):
+    def registra_horas(self, horas=None):
         print(f'Horas registradas...: {horas}\n')
 
     def mostrar_tarefas(self):
@@ -26,7 +26,7 @@ class Alura(Funcionario):
     def busca_perguntas_sem_resposta(self):
         print('Mostrando perguntas não respondidas do fórum')
 
-class hispter():
+class hispter(Funcionario):
     def __str__(self):
         return f'Meu {self.nome}'
     
@@ -34,21 +34,30 @@ class hispter():
 class junior(Alura):
     pass
 
-class pleno(Alura,Caelum):
+class pleno(Caelum,Alura):
     pass
 
 class senior(Alura,Caelum,hispter):
     pass
 
-jose = junior("Carlos")
+
+class todos(senior):
+    pass
+
+jose = junior()
 jose.busca_perguntas_sem_resposta()
 jose.mostrar_tarefas()
 jose.registra_horas("12:00")
 
 luan = pleno("tesouro")
 luan.busca_perguntas_sem_resposta()
+luan.mostrar_tarefas()
 luan.busca_cursos_do_mes("Python")
 luan.registra_horas("13:00")
 
 pedro= senior("garoto")
 print(pedro)
+
+lucas = todos("luana")
+print(lucas)
+lucas.busca_cursos_do_mes("Matematica")
