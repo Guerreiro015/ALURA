54695a425b117166977fb0783cf25edc1186cfae
Guerreiro015 <antoniolucasluana@gmail.com>
2023-06-30 20:11:48 -0300

    Update datas.py

--- PYTHON/OBJETOS/datas.py
+++ PYTHON/OBJETOS/datas.py
@@ -15,6 +15,29 @@ d=data(21,11,2007)
 d.data_formadada()
 
 
+from datetime import date
+
+data = date.today()
+print(data)
+
+datas = f"{data.day}/{data.month}/{data.year}"
+print(datas)
+
+data_certa = data.strftime('%d/%m/%Y') # y maiúsculo para imprimir ano com 4 digitos
+print(data_certa)
+
+from datetime import datetime
+
+data_hora = datetime.now()
+
+horas = data_hora.strftime('%d/%m/%Y')
+print(horas)
+
+horas = data_hora.strftime('%d/%m/%Y %H:%M:%S')
+print(horas)
+
+
+
 
 
 
