0d1d67699bdcd93d169e7601d098928d37aeee09
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-07-12 12:50:42 -0300

    criando altera

--- PYTHON/EXCEL/excel_pandas.py
+++ PYTHON/EXCEL/excel_pandas.py
@@ -4,31 +4,39 @@ os.system("cls" or None)
 
 import pandas as pd
 
-tabela = pd.read_excel("dados.xlsx")
-print(tabela.head())
-print(tabela)
-print(tabela["ID"])
-print(tabela["Nome"])
+# tabela = pd.read_excel("dados.xlsx")
+# print(tabela.head())
+# print(tabela)
+# print(tabela["ID"])
+# print(tabela["Nome"])
 
-#Alterar dados da planilha
-tabela.loc[tabela["ID"] == 112194,"Nome"] = "Antonio de Sousa Neto"
-print(tabela["Nome"],["ID"])
+# #Alterar dados da planilha
+# tabela.loc[tabela["ID"] == 112194,"Nome"] = "Antonio de Sousa Neto"
+# print(tabela["Nome"],["ID"])
 
-#Salvar o arquivo em excel
+# #Salvar o arquivo em excel
+# tabela.to_excel("dados2.xlsx",index=False)
+
+# #+=============================================
+# #Juntando duas planilhas
+# df1 = pd.read_excel("dados.xlsx")
+# df2 = pd.read_excel("dados2.xlsx")
+
+# df3 = pd.concat([df1,df2])
+
+# df3.to_excel("dados_juntos.xlsx")
+# ##################################################
+
+# # Puxando apenas alguas colunas de dados da planilha
+# tabela = pd.read_excel("QUADRO GERAL.xlsx",sheet_name="QUADRO",usecols=(0,1,2,3))
+# tabela.loc[tabela["Cargo"]=="BUEIRISTA","NOME"] = "Contratado para ser Bueirista"
+
+tabela = pd.read_excel("QUADRO GERAL.xlsx",sheet_name="QUADRO")
+print(tabela[["NOME","SALARIO"]]) # para selecionar os campos para ver usas colchetes duplos
+
+tabela.assign(novo_salario = tabela["SALARIO"]*tabela["DIAS"])
+
 tabela.to_excel("dados2.xlsx",index=False)
 
-#+=============================================
-#Juntando duas planilhas
-df1 = pd.read_excel("dados.xlsx")
-df2 = pd.read_excel("dados2.xlsx")
-
-df3 = pd.concat([df1,df2])
-
-df3.to_excel("dados_juntos.xlsx")
-##################################################
-
-# Puxando apenas alguas colunas de dados da planilha
-tabela = pd.read_excel("QUADRO GERAL - ECOSAMPA.xlsx",sheet_name="QUADRO",usecols=(0,1,2,3))
-tabela.loc[tabela["Cargo"]=="BUEIRISTA","NOME"] = "Contratado para ser Bueirista"
-
-print(tabela[["NOME","Cargo","REGIONAL"]])
+nova = pd.read_excel("dados2.xlsx")
+print(nova[["NOME","Cargo","novo_salario"]])
