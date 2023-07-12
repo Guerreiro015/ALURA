fd603314bd3bb35f14172bd1433dc16d098ab3a0
Guerreiro015 <antoniolucasluana@gmail.com>
2023-07-12 20:11:42 -0300

    atuliza

--- PYTHON/EXCEL/excel_pandas.py
+++ PYTHON/EXCEL/excel_pandas.py
@@ -33,12 +33,17 @@ import pandas as pd
 
 tabela = pd.read_excel("QUADRO GERAL.xlsx",sheet_name="QUADRO")
 print(tabela[["ID","NOME","SALARIO"]]) # para selecionar os campos para ver usas colchetes duplos
+ 
+ #INSERIR UMA COLUNA NO FINAL DA PLANILHA DIRETO
 
 tabela["TOTAL"] = tabela["SALARIO"]*tabela["DIAS"]
 
 tabela['total3'] = tabela["SALARIO"]+tabela["DIAS"]
 
+#INSERIR UMA COLUNA USANDO Parmetros ( nº coluna,nome coluna, valor da coluna)
+tabela.insert(8,  "soma",  tabela["DIAS"]+tabela["DIAS"])  
+
 tabela.to_excel("dados2.xlsx",index=False)
 
 nova = pd.read_excel("dados2.xlsx")
-print(nova[["NOME","Cargo","TOTAL","total3"]])
+print(nova[["NOME","Cargo","TOTAL","soma"]])
