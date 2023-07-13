65a27a0064e78bb5e09227a8708cdca82bb739a2
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-07-13 12:53:33 -0300

    atuzliza

--- PYTHON/EXCEL/custo_folha.py
+++ PYTHON/EXCEL/custo_folha.py
@@ -5,24 +5,26 @@ import pandas as pd
 
 
 
-tabela = pd.read_excel("FOLHA05.xlsx")
-print(tabela[["Id Contratado","Nome","Cargo"]]) # para selecionar os campos para ver usas colchetes duplos
+tb = pd.read_excel("FOLHA05.xlsx")
+print(tb[["Id Contratado","Nome","Cargo"]]) # para selecionar os campos para ver usas colchetes duplos
  
  
 #INSERIR UMA COLUNA USANDO Parmetros ( nº coluna,nome coluna, valor da coluna)
-tabela.insert(6,"Selur", tabela["4Salário - Mensalistas"]*0.5/100)
-tabela.insert(6,"Steriiisp", 0)
-tabela.insert(6,"Siemaco", 0)
+tb.insert(6,"Selur", tb["4Salário - Mensalistas"]*0.5/100)
+tb.insert(6,"Steriiisp", 0)
+tb.insert(6,"Siemaco", 0)
+
+tb.insert(9,"Inss",0)
 
 #Alterar dados da planilha
-tabela.loc[tabela["Sindicato"] == "SIEMACO SAO PAULO LIMP URBANA","Siemaco"] = tabela["4Salário - Mensalistas"]*0.6/100
-print(tabela[["Id Contratado","Nome","Siemaco","Steriiisp"]])
+tb.loc[tb["Sindicato"] == "SIEMACO SAO PAULO LIMP URBANA","Siemaco"] = tb["4Salário - Mensalistas"]*0.6/100
+print(tb[["Id Contratado","Nome","Siemaco","Steriiisp"]])
 
-tabela.loc[tabela["Sindicato"] == "SIND TRAB EMP DE ONIBUS RODOV INTEREST INTERM SET DIF SAO PAULO","Steriiisp"] = tabela["4Salário - Mensalistas"]*0.4/100
-print(tabela[["Id Contratado","Nome","Siemaco","Steriiisp"]])
+tb.loc[tb["Sindicato"] == "SIND TRAB EMP DE ONIBUS RODOV INTEREST INTERM SET DIF SAO PAULO","Steriiisp"] = tb["4Salário - Mensalistas"]*0.4/100
+print(tb[["Id Contratado","Nome","Siemaco","Steriiisp"]])
 
 
-tabela.to_excel("CUSTO05.xlsx",index=False)
+tb.to_excel("CUSTO05.xlsx",index=False)
 
 nova = pd.read_excel("CUSTO05.xlsx")
 print("\n------  Cálculos dos sindicatos feito e OK.....  \n")
