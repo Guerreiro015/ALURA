8a64488617756d875d5496ad99a076103a3b6e09
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-07-13 15:06:49 -0300

    atualiza

--- PYTHON/EXCEL/custo_folha.py
+++ PYTHON/EXCEL/custo_folha.py
@@ -14,15 +14,20 @@ tb.insert(6,"Selur", tb["4Salário - Mensalistas"]*0.5/100)
 tb.insert(6,"Steriiisp", 0)
 tb.insert(6,"Siemaco", 0)
 
-tb.insert(9,"Inss",0)
 
 #Alterar dados da planilha
 tb.loc[tb["Sindicato"] == "SIEMACO SAO PAULO LIMP URBANA","Siemaco"] = tb["4Salário - Mensalistas"]*0.6/100
 print(tb[["Id Contratado","Nome","Siemaco","Steriiisp"]])
 
 tb.loc[tb["Sindicato"] == "SIND TRAB EMP DE ONIBUS RODOV INTEREST INTERM SET DIF SAO PAULO","Steriiisp"] = tb["4Salário - Mensalistas"]*0.4/100
+
 print(tb[["Id Contratado","Nome","Siemaco","Steriiisp"]])
 
+tb.insert(9,"Inss",(tb["1001Base INSS 13º Salário"]+tb["1007Base do INSS Normal"]+tb["1008Base do INSS Normal Excedente"]+tb["1031Base INSS/FGTS Férias do Mês"])*28.9854/100)
+
+tb.insert(9,"Fgts",(tb["1005Base do FGTS Normal"]+tb["1010Base do FGTS 13º Salário"]+tb["1031Base INSS/FGTS Férias do Mês"])*8/100-tb["1039Valor do FGTS - GRFF"])
+
+tb.loc[tb["Cargo"] == "MENOR/JOVEM APRENDIZ","Fgts"] = tb["15Salário Aprendiz - Mensalistas"]*2/100
 
 tb.to_excel("CUSTO05.xlsx",index=False)
 
