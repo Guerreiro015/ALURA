15cd0b9d01490f6d69203b42b503f7ed870392d7
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-07-18 12:57:13 -0300

    Update tuplas.py

--- PYTHON/LISTAS/tuplas.py
+++ PYTHON/LISTAS/tuplas.py
@@ -18,3 +18,37 @@ conta = deposito(500)
 print(f"Conta depois do depósito {conta}")
 
 # Conclusão não conseguimos modificar mas conseguimos criar outra tupla com os valores alterados
+
+
+class banco:
+
+  def __init__(self, nome):
+    self._nome = nome
+    self._saldo = 0
+
+  def deposita(self, valor):
+    self._saldo += valor
+
+  def __str__(self):
+    return f"\nNome {self._nome} Saldo {self._saldo}"
+  
+#cliente1 = banco("Antonio")
+print(banco("Antonio"))
+
+class ContaCorrente(banco):
+
+  def passa_o_mes(self):
+    self._saldo -= 2
+
+class ContaCorrente(banco):
+
+  def passa_o_mes(self):
+    self._saldo -= 2
+
+class ContaPoupanca(banco):
+
+  def passa_o_mes(self):
+    self._saldo *= 1.01
+    self._saldo -= 3
+
+print(banco("Antonio"))
