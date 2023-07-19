7848c3d21a9ac4c093fb2b87e795b0d3837819a9
Guerreiro015 <antoniolucasluana@gmail.com>
2023-07-19 05:25:59 -0300

    Create array_numpy.py

---
+++ PYTHON/LISTAS/array_numpy.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+import array as arr
+
+teste = arr.array('d', [1, 3.5])
+
+print(teste)
+# Esta função interna array não é utilizada.
+
+# em vez disso usamos o numpy
+
+import numpy as np
+
+teste = np.array([1.,3.4,5.])# Parenteses e dentro lista com colchetes
+
+print(teste)
+
+tsste=teste+5
+
