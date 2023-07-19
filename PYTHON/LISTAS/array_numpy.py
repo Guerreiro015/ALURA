fdbab595af1914dafc274828e90ef099ab13af33
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-07-19 09:50:11 -0300

    Update array_numpy.py

--- PYTHON/LISTAS/array_numpy.py
+++ PYTHON/LISTAS/array_numpy.py
@@ -18,3 +18,14 @@ print(teste)
 
 tsste=teste+5
 
+
+# Faltou importar o que eu tenho que importar. Para isso,
+#  eu faço from abc import ABCMeta, abstractmethod.
+#  Usamos o ABCmeta como uma meta classe, metaclass.
+#  É uma configuração que precisamos colocar: class Conta(metaclass=ABCMeta):.
+#  O abstractmethod já está no nosso código, mais abaixo.
+
+from abc import ABCMeta, abstractmethod
+
+class Conta(metaclass=ABCMeta):
+
