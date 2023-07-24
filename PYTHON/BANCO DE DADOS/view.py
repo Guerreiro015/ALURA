011f264b05632906bc6a4320e9df8f91bba5bc6c
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-07-24 14:46:05 -0300

    upadate

--- PYTHON/BANCO DE DADOS/view.py
+++ PYTHON/BANCO DE DADOS/view.py
@@ -4,19 +4,33 @@ os.system("cls")
 
 # import sqlite
 import sqlite3 as lite
-
-# Criando banco de dados
 con = lite.connect("dados.bd")
 
-#Inserir dados
 dados = ['vaso','sala','vaso de planta','PvA','13/03/2023','120',"001002",'c:imagem']
+
+#INSERIR DADOS ------------------------------------------------
 cur = con.cursor()
 query = "INSERT INTO inventario(nome, local, descricao, marca, data_compra,valor_compra,serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
-
 cur.execute(query,dados)
 
-#VER DADOS
 
+#ATUALIZAR DADOS -----------------------------------------------------
+atualizar_dados = ['vaso','Cozinha','vaso de planta','PvA','13/03/2023','120',"001002",'c:imagem',1]
+with con:
+    cur = con.cursor()
+    query = "UPDATE inventario SET nome=?, local=?, descricao=?, marca=?, data_compra=?,valor_compra=?,serie=?, imagem=? WHERE id=?"
+    cur.execute(query,atualizar_dados)
+
+
+#DELETAR DADOS -----------------------------------------------------
+deletar_dados = [1]
+with con:
+    cur = con.cursor()
+    query = "DELETE FROM inventario WHERE id = ?"
+    cur.execute(query, deletar_dados[0])
+
+
+#VER DADOS -----------------------------------------------------
 ver_dados = []
 with con:
     cur = con.cursor()
@@ -28,6 +42,7 @@ with con:
     for x in dad:
         ver_dados.append(x)
 
-
 print(ver_dados)
+
+
 
