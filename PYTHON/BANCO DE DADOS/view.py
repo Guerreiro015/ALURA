5b51c6cb3a10b5c5c094964251aedf2fb52b5a59
Guerreiro015 <antoniolucasluana@gmail.com>
2023-07-24 19:12:28 -0300

    atualizar

--- PYTHON/BANCO DE DADOS/view.py
+++ PYTHON/BANCO DE DADOS/view.py
@@ -9,40 +9,62 @@ con = lite.connect("dados.bd")
 dados = ['vaso','sala','vaso de planta','PvA','13/03/2023','120',"001002",'c:imagem']
 
 #INSERIR DADOS ------------------------------------------------
-cur = con.cursor()
-query = "INSERT INTO inventario(nome, local, descricao, marca, data_compra,valor_compra,serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
-cur.execute(query,dados)
+def inserir(valor):
+    cur = con.cursor()
+    query = "INSERT INTO inventario(nome, local, descricao, marca, data_compra,valor_compra,serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
+    cur.execute(query,valor)
 
 
 #ATUALIZAR DADOS -----------------------------------------------------
-atualizar_dados = ['vaso','Cozinha','vaso de planta','PvA','13/03/2023','120',"001002",'c:imagem',1]
-with con:
-    cur = con.cursor()
-    query = "UPDATE inventario SET nome=?, local=?, descricao=?, marca=?, data_compra=?,valor_compra=?,serie=?, imagem=? WHERE id=?"
-    cur.execute(query,atualizar_dados)
+alterar = ['vaso','quarto','vaso de planta','PvA','13/03/2023','120',"001002",'c:imagem',2]
+def atualizar(valor):
+    with con:
+        cur = con.cursor()
+        query = "UPDATE inventario SET nome=?, local=?, descricao=?, marca=?, data_compra=?,valor_compra=?,serie=?, imagem=? WHERE id=?"
+        cur.execute(query,valor)
 
 
 #DELETAR DADOS -----------------------------------------------------
-deletar_dados = [1]
-with con:
-    cur = con.cursor()
-    query = "DELETE FROM inventario WHERE id = ?"
-    cur.execute(query, deletar_dados[0])
+deletar_dados = str(2)
+def apagar(valor):
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM inventario WHERE local = ?"
+        cur.execute(query, valor)
 
 
 #VER DADOS -----------------------------------------------------
 ver_dados = []
-with con:
-    cur = con.cursor()
-    query = "SELECT * FROM inventario"
-    cur.execute(query)
+def visualizar():   
+    with con:
+        cur = con.cursor()
+        query = "SELECT * FROM inventario"
+        cur.execute(query)
 
-    dad = cur.fetchall()
+        dad = cur.fetchall()
 
-    for x in dad:
-        ver_dados.append(x)
+        for x in dad:
+            ver_dados.append(x)
 
-print(ver_dados)
+    print(ver_dados)
 
+ver_dados_individual = []
+def visualizar_individual():   
+    with con:
+        cur = con.cursor()
+        query = "SELECT * FROM inventario"
+        cur.execute(query)
 
+        dad = cur.fetchall()
+
+        for x in dad:
+            ver_dados.append(x)
+
+    print(ver_dados)
+
+#inserir(dados)
+visualizar()
+atualizar(alterar)
+apagar(deletar_dados)
+visualizar()
 
