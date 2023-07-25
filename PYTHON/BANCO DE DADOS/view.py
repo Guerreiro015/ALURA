4b172348c5773a7cdc5e50fa01b70bfd4b533592
Guerreiro015 <antoniolucasluana@gmail.com>
2023-07-24 22:39:49 -0300

    atuiza

--- PYTHON/BANCO DE DADOS/view.py
+++ PYTHON/BANCO DE DADOS/view.py
@@ -16,7 +16,7 @@ def inserir(valor):
 
 
 #ATUALIZAR DADOS -----------------------------------------------------
-alterar = ['vaso','quarto','vaso de planta','PvA','13/03/2023','120',"001002",'c:imagem',2]
+novos_dados = ['vaso','Banheiro','vaso de planta','PvA','13/03/2023','120',"001002",'c:imagem',5]
 def atualizar(valor):
     with con:
         cur = con.cursor()
@@ -25,11 +25,11 @@ def atualizar(valor):
 
 
 #DELETAR DADOS -----------------------------------------------------
-deletar_dados = str(2)
+
 def apagar(valor):
     with con:
         cur = con.cursor()
-        query = "DELETE FROM inventario WHERE local = ?"
+        query = "DELETE FROM inventario WHERE local=?"
         cur.execute(query, valor)
 
 
@@ -46,25 +46,27 @@ def visualizar():
         for x in dad:
             ver_dados.append(x)
 
+    #return(ver_dados)
     print(ver_dados)
 
 ver_dados_individual = []
-def visualizar_individual():   
+id = 0
+def visualizar_individual(valor):   
     with con:
         cur = con.cursor()
-        query = "SELECT * FROM inventario"
-        cur.execute(query)
+        query = "SELECT * FROM inventario WHERE id?"
+        cur.execute(query,valor)
 
-        dad = cur.fetchall()
+        dado = cur.fetchall()
 
-        for x in dad:
-            ver_dados.append(x)
+        for x in dado:
+            ver_dados_individual.append(x)
 
     print(ver_dados)
 
 #inserir(dados)
 visualizar()
-atualizar(alterar)
-apagar(deletar_dados)
-visualizar()
+atualizar(novos_dados)
+apagar(str("0"))
+visualizar_individual(str(1))
 
