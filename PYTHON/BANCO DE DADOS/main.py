981386bc631fd066aa1bfe9452024d2b33540b46
Guerreiro015 <antoniolucasluana@gmail.com>
2023-07-26 20:10:24 -0300

    atualiza

--- PYTHON/BANCO DE DADOS/main.py
+++ PYTHON/BANCO DE DADOS/main.py
@@ -44,8 +44,8 @@ frameCima.grid(row=0,column=0) # posição do quadro
 frameMeio = Frame(janela,width=1043, height=300, bg=co1, pady=20, relief=FLAT)
 frameMeio.grid(row=1,column=0, pady=1, padx=0, sticky=NSEW) # NSEW = Norte, Sul, lEste e Oweste
 
-frameBaixo = Frame(janela,width=1043, height=303, bg=co1, relief=FLAT)
-frameBaixo.grid(row=2,column=0, pady=0, padx=1, sticky=NSEW)
+frameBaixo = Frame(janela,width=1043, height=303, bg=co1,pady=20, relief=FLAT)
+frameBaixo.grid(row=2,column=0, pady=2, padx=0, sticky=NSEW)
 
 # Preparando a imagem - FrameCima-------------------------------
 foto=Image.open("inventario.png")
@@ -112,7 +112,7 @@ foto_atualizar = Image.open("carregar.png")
 foto_atualizar = foto_atualizar.resize((20,20))
 foto_atualizar = ImageTk.PhotoImage(foto_atualizar)
 
-foto_deletar = Image.open("caveira.png")
+foto_deletar = Image.open("lixeira.png")
 foto_deletar = foto_deletar.resize((20,20))
 foto_deletar = ImageTk.PhotoImage(foto_deletar)
 
@@ -122,25 +122,85 @@ foto_ver = ImageTk.PhotoImage(foto_ver)
 
 
 
-
-
-
 # Botão adicionar
 l_adicionar = Button(frameMeio,image=foto_adicionar,width=95,text="  Adicionar".upper(),compound=LEFT,anchor=NW,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
-l_adicionar.place(x=350,y=10)
+l_adicionar.place(x=330,y=10)
 
 # Botão Atualizar
 l_atualizar = Button(frameMeio,image=foto_atualizar,width=95,text="  Atualizar".upper(),compound=LEFT,anchor=NW,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
-l_atualizar.place(x=350,y=50)
+l_atualizar.place(x=330,y=50)
 
 # Botão Deletar
 l_deletar = Button(frameMeio,image=foto_deletar,width=95,text="  Deletar".upper(),compound=LEFT,anchor=NW,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
-l_deletar.place(x=350,y=90)
+l_deletar.place(x=330,y=90)
 
 # Botão Mostrar
 l_ver_item = Button(frameMeio,image=foto_ver,width=95,text="  Ver Ítens".upper(),compound=LEFT,anchor=NW,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
-l_ver_item.place(x=350,y=220)
+l_ver_item.place(x=330,y=220)
 
+
+# Frames dos totais
+l_total = Label(frameMeio,text=" Valor Total dos Ítens ",width=27,height=2,anchor=NW,font=("ivy 10 bold"),bg=co7,fg=co1)
+l_total.place(x=460,y=10)
+l_total = Label(frameMeio,text="",width=15, height=1,anchor=CENTER,font=("ivy 17 bold"),bg=co7,fg=co1)
+l_total.place(x=460,y=30)
+
+
+l_quant = Label(frameMeio,text="",width=15, height=3,anchor=CENTER,font=("ivy 17 bold"),bg=co7,fg=co1)
+l_quant.place(x=460,y=110)
+l_quant = Label(frameMeio,text="   Quantidade Total dos Ítens", height=2,anchor=NW,font=("ivy 10 bold"),bg=co7,fg=co1)
+l_quant.place(x=460,y=112)
+
+#-----------------------------------------------------------
+# tabela -----------------------------------------------------------
+
+# Criando cabeçalo da tabela  duas barras de rolagem with dual scrollbars
+tabela_head = [' Item','Nome',  'Sala/Área','Descrição', 'Marca/Modelo', 'Data da compra','Valor da compra', 'Número de série']
+
+lista_itens = []
+
+global tree
+
+tree = ttk.Treeview(frameBaixo, selectmode="extended",columns=tabela_head, show="headings")
+
+# vertical scrollbar -- Barra de rolagem
+vsb = ttk.Scrollbar(frameBaixo, orient="vertical", command=tree.yview)
+
+# horizontal scrollbar -- Barra de rolagem
+hsb = ttk.Scrollbar(frameBaixo, orient="horizontal", command=tree.xview)
+
+tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+tree.grid(column=0, row=0, sticky='nsew')
+vsb.grid(column=1, row=0, sticky='ns')
+hsb.grid(column=0, row=1, sticky='ew')
+frameBaixo.grid_rowconfigure(0, weight=12)
+
+hd=["center","center","center","center","center","center","center", 'center']
+h=[40,150,100,160,130,100,100, 100]
+n=0
+
+for col in tabela_head:
+    tree.heading(col, text=col.title(), anchor=CENTER)
+    # ajusta a largura da coluna para a string do cabeçalho
+    tree.column(col, width=h[n],anchor=hd[n])
+    n+=1
+
+
+# inserindo os itens dentro da tabela
+for item in lista_itens:
+    tree.insert('', 'end', values=item)
+
+
+quantidade = [8888,88]
+
+for iten in lista_itens:
+    quantidade.append(iten[6])
+
+Total_valor = sum(quantidade)
+Total_itens = len(quantidade)
+
+l_total['text'] = f'R$ {Total_valor:,.2f}'
+l_quant['text'] = Total_itens
 
 
 
