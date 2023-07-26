e42f488fa33e62fb933a172fef7b3bb2fb8f0f91
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-07-26 15:37:51 -0300

    updates

--- PYTHON/BANCO DE DADOS/main.py
+++ PYTHON/BANCO DE DADOS/main.py
@@ -48,7 +48,7 @@ frameBaixo = Frame(janela,width=1043, height=303, bg=co1, relief=FLAT)
 frameBaixo.grid(row=2,column=0, pady=0, padx=1, sticky=NSEW)
 
 # Preparando a imagem - FrameCima-------------------------------
-foto=Image.open("ator-1.png")
+foto=Image.open("inventario.png")
 foto = foto.resize((45,45))
 foto = ImageTk.PhotoImage(foto)
 
@@ -59,61 +59,86 @@ foto_logo.place(x=0,y=0)
 
 l_nome = Label(frameMeio,text="Nome", height=1,anchor=NW,font=("ivy 10 bold"),bg=co1,fg=co4)
 l_nome.place(x=10,y=10)
-e_nome = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)# pODEMOS USAR SOLID OU ("solid")
+e_nome = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)# PODEMOS USAR SOLID OU ("solid")
 e_nome.place(x=130,y=11)
 
 l_local = Label(frameMeio,text="Local/Área", height=1,anchor=NW,font=("ivy 10 bold"),bg=co1,fg=co4)
 l_local.place(x=10,y=40)
-e_local = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)# pODEMOS USAR SOLID OU ("solid")
+e_local = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)
 e_local.place(x=130,y=41)
 
 l_descricao = Label(frameMeio,text="Descrição", height=1,anchor=NW,font=("ivy 10 bold"),bg=co1,fg=co4)
 l_descricao.place(x=10,y=70)
-e_descricao = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)# pODEMOS USAR SOLID OU ("solid")
+e_descricao = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)
 e_descricao.place(x=130,y=71)
 
 l_marca = Label(frameMeio,text="Marca/Modelo", height=1,anchor=NW,font=("ivy 10 bold"),bg=co1,fg=co4)
 l_marca.place(x=10,y=100)
-e_marca = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)# pODEMOS USAR SOLID OU ("solid")
+e_marca = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID))
 e_marca.place(x=130,y=101)
 
 l_compra = Label(frameMeio,text="Data da Compra", height=1,anchor=NW,font=("ivy 10 bold"),bg=co1,fg=co4)
 l_compra.place(x=10,y=130)
-# Criando um calendario)-----------------------------------------
+# CRIANDO UM CALENDARIO-----------------------------------------
 e_compra = DateEntry(frameMeio,width=12,BackGround='darkblue',bordwidth=2,year=2023)# Criando um calendario
 e_compra.place(x=130,y=131)
+#---------------------------------------------------------------------------------------
+
 
 l_valor = Label(frameMeio,text="Valor da Compra", height=1,anchor=NW,font=("ivy 10 bold"),bg=co1,fg=co4)
 l_valor.place(x=10,y=160)
-e_valor = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)# pODEMOS USAR SOLID OU ("solid")
+e_valor = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)
 e_valor.place(x=130,y=161)
 
 l_serie = Label(frameMeio,text="Número de Serie", height=1,anchor=NW,font=("ivy 10 bold"),bg=co1,fg=co4)
 l_serie.place(x=10,y=190)
-e_serie = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)# pODEMOS USAR SOLID OU ("solid")
+e_serie = Entry(frameMeio,width=30,justify=LEFT,relief=SOLID)
 e_serie.place(x=130,y=191)
 
 #Criando botões --------------------------------------------
 l_img_item = Label(frameMeio,text="Imagem do ítem", compound=CENTER,anchor=NW,font=("ivy 10 bold"),bg=co1,fg=co0)
 l_img_item.place(x=10,y=220)
 
-l_img_item = Button(frameMeio,width=25,text="Imagem do ítem".upper(), height=1,anchor=CENTER,overrelief=RIDGE,font=("ivy 8 bold"),bg=co7,fg=co4)
+l_img_item = Button(frameMeio,width=25,text="Imagem do ítem".upper(), height=1,anchor=CENTER,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
 l_img_item.place(x=130,y=221)
 
+
+#____________________________________
+foto_adicionar = Image.open("Adicionar.png")
+foto_adicionar = foto_adicionar.resize((20,20))
+foto_adicionar = ImageTk.PhotoImage(foto_adicionar)
+
+foto_atualizar = Image.open("carregar.png")
+foto_atualizar = foto_atualizar.resize((20,20))
+foto_atualizar = ImageTk.PhotoImage(foto_atualizar)
+
+foto_deletar = Image.open("caveira.png")
+foto_deletar = foto_deletar.resize((20,20))
+foto_deletar = ImageTk.PhotoImage(foto_deletar)
+
+foto_ver = Image.open("ver.png")
+foto_ver = foto_ver.resize((20,20))
+foto_ver = ImageTk.PhotoImage(foto_ver)
+
+
+
+
+
+
 # Botão adicionar
-l_adicionar = Button(frameMeio,width=15,text="Adicionar".upper(), height=1,anchor=CENTER,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
+l_adicionar = Button(frameMeio,image=foto_adicionar,width=95,text="  Adicionar".upper(),compound=LEFT,anchor=NW,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
 l_adicionar.place(x=350,y=10)
 
-# Botão Atulizar
-l_atualizar = Button(frameMeio,width=15,text="atualizar".upper(), height=1,anchor=CENTER,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
+# Botão Atualizar
+l_atualizar = Button(frameMeio,image=foto_atualizar,width=95,text="  Atualizar".upper(),compound=LEFT,anchor=NW,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
 l_atualizar.place(x=350,y=50)
 
 # Botão Deletar
-l_deletar = Button(frameMeio,width=15,text="deletar".upper(), height=1,anchor=CENTER,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
+l_deletar = Button(frameMeio,image=foto_deletar,width=95,text="  Deletar".upper(),compound=LEFT,anchor=NW,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
 l_deletar.place(x=350,y=90)
 
 # Botão Mostrar
-l_ver_item = Button(frameMeio,width=15,text="Ver Ítem".upper(), height=1,anchor=CENTER,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
+l_ver_item = Button(frameMeio,image=foto_ver,width=95,text="  Ver Ítens".upper(),compound=LEFT,anchor=NW,overrelief=RIDGE,font=("ivy 8 bold"),bg=co1,fg=co4)
 l_ver_item.place(x=350,y=220)
 
 
