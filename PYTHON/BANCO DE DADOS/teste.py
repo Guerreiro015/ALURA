2d627e2381703cd77d3cf3ae86bb33f0b6117a36
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-08-11 16:34:04 -0300

    Update teste.py

--- PYTHON/BANCO DE DADOS/teste.py
+++ PYTHON/BANCO DE DADOS/teste.py
@@ -53,13 +53,13 @@ style = ttk.Style(janela)
 style.theme_use("clam")
 
 
-frameCima = Frame(janela,width=1043, height=50, bg=co8, relief=FLAT) #Tamanho do quadro
+frameCima = Frame(janela,width=1043, height=30, bg=co8, relief=FLAT) #Tamanho do quadro
 frameCima.grid(row=0,column=0) # posição do quadro
 
 texto_cima= Label(frameCima, text='  TABELAS DE IMPOSTO DE RENDAS e INSS ', width=600, compound=CENTER, relief=RAISED, anchor=SW, font=('verdana 15 bold'),bg=co9,fg=co2)
 texto_cima.place(x=0,y=0)
 
-frameMeio = Frame(janela,width=1043, height=330, bg=co7, pady=20, relief=FLAT)
+frameMeio = Frame(janela,width=1043, height=280, bg=co7, pady=20, relief=FLAT)
 frameMeio.grid(row=1,column=0, pady=1, padx=0, sticky=NSEW) # NSEW = Norte, Sul, lEste e Oweste
 
 framebaixo = Frame(janela,width=1043, height=303, bg=co3,pady=20, relief=FLAT)
@@ -68,69 +68,135 @@ framebaixo.grid(row=2,column=0, pady=2, padx=0, sticky=NSEW)
 
 
 l_salario = Label(frameMeio, text='  TABELA DE INSS  ', font=('verdana 13 bold'),bg=co7,fg=co1)
-l_salario.place(x=0,y=0)
-l_insalu = Label(frameMeio, text='  Insalubridade', font=('verdana 10 bold'),bg=co7,fg=co1)
-l_insalu.place(x=0,y=25)
-l_pericu = Label(frameMeio, text='  Periculosidade', font=('verdana 10 bold'),bg=co7,fg=co1)
-l_pericu.place(x=0,y=50)
-l_he = Label(frameMeio, text='  Horas Extras ', font=('verdana 10 bold'),bg=co7,fg=co1)
-l_he.place(x=0,y=75)
-l_adno = Label(frameMeio, text='  Adicional Noturno ', font=('verdana 10 bold'),bg=co7,fg=co1)
-l_adno.place(x=0,y=100)
-l_dsr = Label(frameMeio, text='  DSR  - (H.E - Adic.Notur) ', font=('verdana 10 bold'),bg=co7,fg=co1)
-l_dsr.place(x=0,y=125)
-l_outros = Label(frameMeio, text='  Outros Proventos  ', font=('verdana 10 bold'),bg=co7,fg=co1)
-l_outros.place(x=0,y=150)
+l_salario.place(x=150,y=0)
+l_insalu = Label(frameMeio, text='  Salário de Contribuição', font=('verdana 11 bold'),bg=co7,fg=co3)
+l_insalu.place(x=135,y=25)
 
-
-e_insalu = Entry(frameMeio,width=20,justify=LEFT,relief=SOLID)
-e_insalu.place(x=200,y=25)
-e_pericu = Entry(frameMeio,width=20,justify=LEFT,relief=SOLID)
-e_pericu.place(x=200,y=50)
-e_he = Entry(frameMeio,width=20,justify=LEFT,relief=SOLID)
-e_he.place(x=200,y=75)
-e_adno = Entry(frameMeio,width=20,justify=LEFT,relief=SOLID)
-e_adno.place(x=200,y=100)
-e_dsr = Entry(frameMeio,width=20,justify=LEFT,relief=SOLID)
-e_dsr.place(x=200,y=125)
-e_outros = Entry(frameMeio,width=20,justify=LEFT,relief=SOLID)
-e_outros.place(x=200,y=150)
-e_salario = Entry(frameMeio,width=20,justify=LEFT,relief=SOLID)
-e_salario.place(x=200,y=175)
-
-
-e_faltas = Entry(frameMeio,width=20,justify=LEFT,relief=SOLID)
-e_faltas.place(x=200,y=225)
-e_depe = Entry(frameMeio,width=10,justify=LEFT,relief=SOLID)
-e_depe.place(x=200,y=250)
-e_pensao = Entry(frameMeio,width=20,justify=LEFT,relief=SOLID)
-e_pensao.place(x=200,y=275)
+l_pericu = Label(frameMeio, text='  DE', font=('verdana 10 bold'),bg=co7,fg=co1)
+l_pericu.place(x=50,y=50)
+l_pericu = Label(frameMeio, text='  ATÉ ', font=('verdana 10 bold'),bg=co7,fg=co1)
+l_pericu.place(x=150,y=50)
+l_pericu = Label(frameMeio, text=' Áliquota ', font=('verdana 10 bold'),bg=co7,fg=co1)
+l_pericu.place(x=230,y=50)
+l_pericu = Label(frameMeio, text='Parcela P/ Deduzir', font=('verdana 10 bold'),bg=co7,fg=co1)
+l_pericu.place(x=330,y=50)
 
 
 
 
-campos = [e_salario,e_insalu,e_pericu,e_he,e_adno,e_dsr,e_faltas,e_depe,e_pensao,e_outros]
 
-def atualizar():
-    
-    try:
-        for i in campos:
-            if i.get() == '':
-                messagebox.showinfo('Erro', 'Preencha todos os campos')
-                return
-        sal = float(e_salario.get())
-        ins = float(e_insalu.get())
-        per = float(e_pericu.get())
-        hex = float(e_he.get())
-        adn = float(e_adno.get())
-        ds = float(e_dsr.get())
-        fal = float(e_faltas.get())
-        pen = float(e_pensao.get())
-        dep = float(e_depe.get())
-        out = float(e_outros.get()) 
-        
-    except:
-            messagebox.showerror('Erro', 'Por favor Verifique, A entrada de dados contém ERROS') 
+de1 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+de1.place(x=30,y=70)
+ate1 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+ate1.place(x=130,y=70)
+ali1 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+ali1.place(x=230,y=70)
+par1 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+par1.place(x=330,y=70)
+
+de2 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+de2.place(x=30,y=90)
+ate2 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+ate2.place(x=130,y=90)
+ali2 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+ali2.place(x=230,y=90)
+par2 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+par2.place(x=330,y=90)
+
+de3 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+de3.place(x=30,y=110)
+ate3 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+ate3.place(x=130,y=110)
+ali3 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+ali3.place(x=230,y=110)
+par3 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+par3.place(x=330,y=110)
+
+de4 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+de4.place(x=30,y=130)
+ate4 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+ate4.place(x=130,y=130)
+ali4 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+ali4.place(x=230,y=130)
+par4 = Entry(frameMeio,width=15,justify=LEFT,relief=SOLID)
+par4.place(x=330,y=130)
+
+
+
+
+l_salario = Label(framebaixo, text='  TABELA DE IRRF  ', font=('verdana 13 bold'),bg=co3,fg=co1)
+l_salario.place(x=150,y=0)
+l_insalu = Label(framebaixo, text=' Base de Cálculo de IRRF ', font=('verdana 11 bold'),bg=co3,fg=co1)
+l_insalu.place(x=135,y=25)
+
+l_pericu = Label(framebaixo, text='  DE', font=('verdana 10 bold'),bg=co3,fg=co1)
+l_pericu.place(x=50,y=50)
+l_pericu = Label(framebaixo, text='  ATÉ ', font=('verdana 10 bold'),bg=co3,fg=co1)
+l_pericu.place(x=150,y=50)
+l_pericu = Label(framebaixo, text=' Áliquota ', font=('verdana 10 bold'),bg=co3,fg=co1)
+l_pericu.place(x=230,y=50)
+l_pericu = Label(framebaixo, text='Parcela P/ Deduzir', font=('verdana 10 bold'),bg=co3,fg=co1)
+l_pericu.place(x=330,y=50)
+
+dependente = 189.59
+deducao_simplicada = 528.00
+l_pericu = Label(framebaixo, text=f'Dedução Por Dependente .......... ... : R$: {dependente: .2f}', font=('verdana 10 bold'),bg=co3,fg=co1)
+l_pericu.place(x=30,y=180)
+l_pericu = Label(framebaixo, text=f'Base de DEDUÇÃO SIMPLIFICADA...:  R$: {deducao_simplicada: .2f}', font=('verdana 10 bold'),bg=co3,fg=co1)
+l_pericu.place(x=30,y=200)
+
+
+
+
+
+de1 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+de1.place(x=30,y=70)
+ate1 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ate1.place(x=130,y=70)
+ali1 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ali1.place(x=230,y=70)
+par1 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+par1.place(x=330,y=70)
+
+de2 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+de2.place(x=30,y=90)
+ate2 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ate2.place(x=130,y=90)
+ali2 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ali2.place(x=230,y=90)
+par2 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+par2.place(x=330,y=90)
+
+de3 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+de3.place(x=30,y=110)
+ate3 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ate3.place(x=130,y=110)
+ali3 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ali3.place(x=230,y=110)
+par3 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+par3.place(x=330,y=110)
+
+de4 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+de4.place(x=30,y=130)
+ate4 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ate4.place(x=130,y=130)
+ali4 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ali4.place(x=230,y=130)
+par4 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+par4.place(x=330,y=130)
+
+de5= Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+de5.place(x=30,y=150)
+ate5 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ate5.place(x=130,y=150)
+ali5 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+ali5.place(x=230,y=150)
+par5 = Entry(framebaixo,width=15,justify=LEFT,relief=SOLID)
+par5.place(x=330,y=150)
+
+
+
+
 
 
 janela.mainloop()
