11b157031f0b5be4ac481e161d3343539f008073
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-08-24 09:51:12 -0300

    uodfate

--- PYTHON/PROJETOS COPÍADOS/gabarito.py
+++ PYTHON/PROJETOS COPÍADOS/gabarito.py
@@ -23,7 +23,7 @@ texto.grid(column=0, row=0, padx=10, pady=10)
 botao = Button(janela, text="Buscar cotações", command=pegar_cotacoes)
 botao.grid(column=0, row=1, padx=10, pady=10)
 
-texto_resposta = Label(janela, text="")
+texto_resposta = Label(janela)
 texto_resposta.grid(column=0, row=2, padx=10, pady=10)
 
 
--- PYTHON/importacao.py
+++
@@ -1,6 +0,0 @@
-# Vamos puxar uma função criada em outro programa
-
-import modulo_titulo # importando um programa voce pode usar suas funcoes
-
-modulo_titulo.menu() # usando funcao que esta no programa titulo_menu
-
