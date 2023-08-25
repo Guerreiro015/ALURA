395fc53edd407aa097fd92d7d0f404ae459800e5
Guerreiro015 <129054827+Guerreiro015@users.noreply.github.com>
2023-08-25 15:19:36 -0300

    Update salao.py

--- PYTHON/PROJETOS PYTHON/salao.py
+++ PYTHON/PROJETOS PYTHON/salao.py
@@ -9,7 +9,7 @@ from tkcalendar import Calendar,DateEntry
 import sqlite3
 
 janela= tkinter.Tk()
-janela.title('Salão de Beleza')
+janela.title('Empresa Ficticia')
 #janela.geometry('800x400')
 frame = tkinter.Frame(janela)
 frame.pack()
@@ -38,32 +38,52 @@ email_label.grid(row=0,column=3)
 email_entry=tkinter.Entry(frame1)
 email_entry.grid(row=1,column=3)
 
-frame4=tkinter.LabelFrame(frame,text="Dados Endereço")
+for widget in frame1.winfo_children():
+    widget.grid_configure(padx=10,pady=5)
+
+
+frame4=tkinter.LabelFrame(frame,text="Dados Endereço:")
+frame4.grid(row=1,column=0)
 
 xxx=tkinter.Label(frame1,text='Endereço:')
 xxx.grid(row=2,column=0)
-xxx=tkinter.Button(frame1,text='Consultar CEP:')
-xxx.grid(row=2,column=1)
+xxx1=tkinter.Label(frame1,text='CEP:')
+xxx1.grid(row=2,column=1)
+xxx3=tkinter.Entry(frame1)
+xxx3.grid(row=2,column=2)
+xxx4=tkinter.Button(frame1,text='Consultar CEP:',bg='Blue',fg='Yellow')
+xxx4.grid(row=2,column=3)
 
-rua_label = tkinter.Label(frame1,text='Rua')
+
+rua_label = tkinter.Label(frame1,text='Rua:')
 rua_label.grid(row=3,column=0)
 rua_entry = tkinter.Entry(frame1)
 rua_entry.grid(row=4, column=0)
 
-numero_label = tkinter.Label(frame1,text='Número')
+numero_label = tkinter.Label(frame1,text='Número:')
 numero_label.grid(row=3,column=1)
 numero_entry = tkinter.Entry(frame1)
 numero_entry.grid(row=4, column=1)
 
-bairro_label = tkinter.Label(frame1,text='Bairro')
+bairro_label = tkinter.Label(frame1,text='Bairro:')
 bairro_label.grid(row=3,column=2)
 bairro_entry = tkinter.Entry(frame1)
 bairro_entry.grid(row=4, column=2)
 
-cidade_label = tkinter.Label(frame1,text='Cidade')
+cidade_label = tkinter.Label(frame1,text='Cidade:')
 cidade_label.grid(row=3,column=3)
 cidade_entry = tkinter.Entry(frame1)
 cidade_entry.grid(row=4, column=3)
+
+estado_label = tkinter.Label(frame1,text='Estado:')
+estado_label.grid(row=5,column=0)
+estado_label_entry = tkinter.Entry(frame1)
+estado_label_entry.grid(row=6, column=0)
+
+uf_label = tkinter.Label(frame1,text='UF:')
+uf_label.grid(row=5,column=1)
+uf_entry = tkinter.Entry(frame1)
+uf_entry.grid(row=6, column=1)
 
 
 
