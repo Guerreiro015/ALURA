97181d5cad972a188831307f4b5d4223c98be897
Guerreiro015 <antoniolucasluana@gmail.com>
2023-09-12 21:33:29 -0300

    uodate

--- SITES DJANGO/usuarios/views.py
+++ SITES DJANGO/usuarios/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from usuarios.forms import LoginForms
+from usuarios.forms import CadastroForms
 
 def login(request):
-    return render(request, 'usuarios/login.html')
+        form = LoginForms()
+        return render(request, "usuarios/login.html", {"form": form})  # Este codigo e para mostrar o formulário feito no arquivo form
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+        form=CadastroForms
+        return render(request, "usuarios/cadastro.html",{"form": form}) # Este codigo e para mostrar o formulário feito no arquivo form
