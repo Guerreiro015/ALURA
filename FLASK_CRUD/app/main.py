49263030ce5dc92d02621e328a064a4db645e11c
Guerreiro015 <antoniolucasluana@gmail.com>
2023-09-30 06:03:31 -0300

    rrr

--- FLASK_CRUD/app/main.py
+++ FLASK_CRUD/app/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from database import db
 from flask import render_template
 from flask_migrate import Migrate
+from usuarios import bp_usuarios
 
 app = Flask(__name__)
 db.init_app(app)
@@ -10,6 +11,8 @@ conexao = 'sqlite:///meubanco.sqlite'
 app.config['SECRET_KEY'] = 'base-chave-nova'
 app.config['SQLALCHEMY_DATABASE_URL'] = conexao
 app.config['SQLALCHEMY_TRACKMODIFICATIONS'] = False
+app.register_blueprint(bp_usuarios, url_prefix='/usuarios')
+
 
 Migrate = Migrate(app,db)
 
@@ -18,8 +21,8 @@ Migrate = Migrate(app,db)
 
 
 @app.route("/") 
-def homepage():
-    return render_template('index.html')
+def index():
+    return ('Ola testado o minfol')
 
 
 
