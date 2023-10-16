b8a703482d1d84ed3bd647747e67ff4eccf5ac4e
Guerreiro015 <antoniolucasluana@gmail.com>
2023-10-15 22:24:12 -0300

    crud_alura

--- Flask_CLUD - ALURA/venv/Scripts/activate.bat
+++ Flask_CLUD - ALURA/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Luana\Documents\GitHub\ALURA\Flask_CLUD - ALURA\venv
+set VIRTUAL_ENV=C:\Users\Luana\Documents\GitHub\ALURA\Flask_CLUD Alura\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
